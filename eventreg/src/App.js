218bc992f3567f9c7afb9f475101eced92860bcd
Advika2069 <advikakanchanapally@gmail.com>
2026-02-17 14:48:18 +0530

    mes

---
+++ eventreg/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import  Navbar from './Components/Navbar';
+function App() {
+  return (
+    <>
+    <Navbar title ="Event Registration App" name="Home" />
+    </>
+  );
+}
+
+export default App;
